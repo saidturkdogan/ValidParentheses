ff1453406279a4219ee3a16fe4900c92be4cb0e0
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2023-01-28 03:23:09 +0300

    Add project files.

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             bool IsValid(string s)
+            {
+                s = "()[]{}";
+                Stack<char> stack = new Stack<char>();
+                Dictionary<char, char> dict = new Dictionary<char, char>() {
+            {'(',')'},
+            {'[',']'},
+            {'{','}'},
+        };
+
+                foreach (var c in s)
+                {
+                    if (c == ')' || c == ']' || c == '}')
+                    {
+                        if (stack.Count > 0 && dict[stack.Peek()] != c || stack.Count == 0)
+                        {
+                            return false;
+                        }
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(c);
+                    }
+
+                }
+                return stack.Count == 0;
+
+            }
+        }
+    }
+}
